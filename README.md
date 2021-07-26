59df28190c6763b62934fa7fdc7122ff1724cef6
Renato <tato_ap@hotmail.com>
2021-07-26 11:08:37 -0300

    Preparando projeto e configurando bd

---
+++ README.md
@@ -0,0 +1,16 @@
+## Um crud de api em Node.js
+
+#### Dependências usadas 
+
+- npm install express
+
+- npm install sequelize
+
+- npm install sequelize-cli -d
+
+- npm install mysql2
+
+- npm install nodemon
+
+  
+
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    username: 'root',
+    password: 'matrix1982*',
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    database: 'teste',
+    define: {
+        timestamps: false,
+        underscored: true
+    }
+}
